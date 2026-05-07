d0c054d42bf265fb9de4ac20bdb7f8c578106a90
vriegert <riegertvictor@gmail.com>
2026-05-07 10:19:04 +0200

    feat: add NavigationTarget sealed interface (Block / Column)

---
+++ src/common/src/main/java/fr/riege/ebsl/common/pathfinding/goal/NavigationTarget.java
@@ -0,0 +1,6 @@
+package fr.riege.ebsl.common.pathfinding.goal;
+
+public sealed interface NavigationTarget {
+    record Block(int x, int y, int z) implements NavigationTarget {}
+    record Column(int x, int z)       implements NavigationTarget {}
+}
