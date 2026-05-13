e362f95d204d75ef66844a627d9578865b2207b7
vriegert <riegertvictor@gmail.com>
2026-05-13 16:25:03 +0200

    build(license): enforce java source headers

--- modules/app/src/main/java/fr/riege/ebsl/common/feature/ui/imgui/ImGuiPanelUtil.java
+++ modules/app/src/main/java/fr/riege/ebsl/common/feature/ui/imgui/ImGuiPanelUtil.java
@@ -1,3 +1,16 @@
+/*
+ * This file is part of EBSL.
+ *
+ * EBSL is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * EBSL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 package fr.riege.ebsl.common.feature.ui.imgui;
 
 import fr.riege.ebsl.common.feature.ui.layout.UiRect;
